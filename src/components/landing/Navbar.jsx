4b1d0cb487e66836dc6c9c0edd490e99e0cce53c
SebasXPG <juanusugas18@gmail.com>
2026-02-25 22:04:51 -0500

    Se agrega el enlace al boton de login en ladingPages

--- src/components/landing/Navbar.jsx
+++ src/components/landing/Navbar.jsx
@@ -33,7 +33,7 @@ const Navbar = () => {
           </li>
         </ul>
 
-        <NavLink to="/en-construccion" className="navbar-btn">
+        <NavLink to="/login" className="navbar-btn">
           <svg className="btn-icon" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
             <path d="M15 3h4a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-4"/>
             <polyline points="10 17 15 12 10 7"/>
